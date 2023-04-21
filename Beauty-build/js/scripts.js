2ac4530f8c305c07d69f9e5374e045c40976e746
Daniel-spec-dell <63346599+Daniel-spec-dell@users.noreply.github.com>
2023-04-21 11:46:28 +0300

    Beauty-site upload files

---
+++ Beauty-build/js/scripts.js
@@ -0,0 +1,37 @@
+// Custom Scripts
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu()
+
+const textField = document.querySelector('.text-field-navigation-cont__text-field');
+const textFieldIcon = document.querySelector('.text-field-icon');
+
+textField.addEventListener('mouseover', function () {
+    textFieldIcon.classList.add('dis-active');
+})
+textField.addEventListener('mouseout', function () {
+    textFieldIcon.classList.remove('dis-active');
+})
